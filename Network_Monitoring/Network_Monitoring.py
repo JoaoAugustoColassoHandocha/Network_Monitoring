0993145f5ac6afc274652356645c08cb643a5a07
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-06-25 13:19:09 +0000

    Update

--- Network_Monitoring/Network_Monitoring.py
+++ Network_Monitoring/Network_Monitoring.py
@@ -6,17 +6,17 @@ import csv
 # List of IP addresses or hostnames of 100 computers.
 computers = [f'0.0.0.{i}' for i in range(2, 103)]  # Example of IPs
 
+with open('NetworkMonitoring.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['IP','STATUS', 'DATE | TIME'])
+
 # Function to ping a computer
 def check_ping(ip):
     
-    # Store the status in a CSV file.
-    with open('NetworkMonitoring.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['IP','STATUS', 'DATE | TIME'])
-    
     response = os.system(f'ping -n 1 -W 1000 {ip}')
     status = 'Online' if response == 0 else 'Offline'
-
+    
+    # Store the status in a CSV file.
     with open('NetworkMonitoring.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([ip, status, time.strftime('%Y-%m-%d | %H:%M:%S')])
