2623d8e6cf6ee04d08d6213a14dccaec9024f463
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-06-25 13:50:34 +0000

    Update

--- Network_Monitoring/Network_Monitoring.py
+++ Network_Monitoring/Network_Monitoring.py
@@ -6,11 +6,12 @@ import csv
 # List of IP addresses or hostnames of 100 computers.
 computers = [f'0.0.0.{i}' for i in range(2, 103)]  # Example of IPs
 
+# Creation of the CSV file.
 with open('NetworkMonitoring.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['IP','STATUS', 'DATE | TIME'])
 
-# Function to ping a computer
+# Function to ping a computer.
 def check_ping(ip):
     
     response = os.system(f'ping -n 1 -W 1000 {ip}')
@@ -28,7 +29,7 @@ def monitor_computers():
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
         pool.map(check_ping, computers)
 
-# Monitor periodically
+# Monitor periodically.
 while True:
     monitor_computers()
-    time.sleep(60)  # Check every 60 seconds
+    time.sleep(60)  # Check every 60 seconds.
